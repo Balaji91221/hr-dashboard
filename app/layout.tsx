37bea5fcaacc551aa938a1d0715256e955a4cb7d
Balaji91221 <kbalaji15j@gmail.com>
2025-07-02 22:44:47 +0530

    setup base folders and layout  add dark/light mode toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+   <html className="dark">
+  <body className="bg-white dark:bg-black text-black dark:text-white">
+    {children}
+  </body>
+</html>
   );
 }
